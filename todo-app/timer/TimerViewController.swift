6ec424b30de06de15db466299a3f97a7b91827d8
Tomáš Kacálek <kacalek.tom@gmail.com>
2018-09-10 17:46:45 +0200

    dark mode

--- todo-app/timer/TimerViewController.swift
+++ todo-app/timer/TimerViewController.swift
@@ -10,26 +10,48 @@ import UIKit
 
 class TimerViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var pauseBtn: UIButton!
+    @IBOutlet weak var resetBtn: UIButton!
+    
+    @IBAction func playBtnClick(_ sender: Any) {
+        if (!isTimeRunning){
+            runTimer()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pauseBtnClick(_ sender: Any) {
+        isTimeRunning = !isTimeRunning
+    }
+    @IBAction func resetBtnClick(_ sender: Any) {
+        timeLabel.text = "01:00"
+        timer = Timer()
+        seconds = 60
+        isTimeRunning = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    var seconds = 60
+    var timer = Timer()
+    var isTimeRunning = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        timeLabel.text = "01:00"
     }
-    */
-
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerViewController.updateTimer), userInfo: nil, repeats: true)
+        isTimeRunning = true
+    }
+    
+    @objc func updateTimer() {
+        if(seconds <= 0){
+            print("Konec")
+            timeLabel.text = "KONEC"
+        }else if(isTimeRunning){
+            seconds -= 1
+            timeLabel.text = "\(seconds)"
+        }
+    }
 }
