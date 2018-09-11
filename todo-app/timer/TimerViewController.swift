826aae300a503935b9adbd899e295091d029798e
Tomáš Kacálek <kacalek.tom@gmail.com>
2018-09-11 12:57:38 +0200

    progress

--- todo-app/timer/TimerViewController.swift
+++ todo-app/timer/TimerViewController.swift
@@ -17,41 +17,66 @@ class TimerViewController: UIViewController {
     @IBOutlet weak var resetBtn: UIButton!
     
     @IBAction func playBtnClick(_ sender: Any) {
-        if (!isTimeRunning){
-            runTimer()
-        }
     }
     @IBAction func pauseBtnClick(_ sender: Any) {
-        isTimeRunning = !isTimeRunning
+        
     }
     @IBAction func resetBtnClick(_ sender: Any) {
-        timeLabel.text = "01:00"
-        timer = Timer()
-        seconds = 60
-        isTimeRunning = false
+        
     }
     
-    var seconds = 60
-    var timer = Timer()
-    var isTimeRunning = false
+    var timer: MyTimer? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        timeLabel.text = "01:00"
+        timer = MyTimer(label: timeLabel)
     }
     
     func runTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerViewController.updateTimer), userInfo: nil, repeats: true)
-        isTimeRunning = true
+        if let time = timer {
+            time.start()
+        }
+    }
+}
+
+class MyTimer {
+    var seconds = 60
+    var timer: Timer? = nil
+    var isRuning = false
+    var outputLabel: UILabel? = nil
+    
+    init(label: UILabel) {
+        outputLabel = label
+    }
+    
+    func start() {
+        if (!isRuning){
+            isRuning = true
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(MyTimer.updateTimer), userInfo: nil, repeats: true)
+        }
     }
     
     @objc func updateTimer() {
         if(seconds <= 0){
-            print("Konec")
-            timeLabel.text = "KONEC"
-        }else if(isTimeRunning){
+            if let label = outputLabel {
+                label.text = "KONEC"
+            }
+        }else {
             seconds -= 1
-            timeLabel.text = "\(seconds)"
+            if let label = outputLabel {
+                label.text = "\(seconds)"
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
